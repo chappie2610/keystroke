bcf22999a0221c7ff30830c4d918b0506393a299
Chappie <39955916+chappie2610@users.noreply.github.com>
2022-05-01 12:40:33 +0800

    Update script.js

--- webadmin/script.js
+++ webadmin/script.js
@@ -1,6 +1,6 @@
 const $icon = $('.ui-circle');
 const handleIconClick = () => {
-  alert('hellow friendz script1337 is here!!!');
+  alert('hellow friendz Chappie is here!!!');
 };
 
 $icon.on('click', handleIconClick);
